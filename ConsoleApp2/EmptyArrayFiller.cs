a87ad4d5c75c0c6b971833af4dacc90463b58b90
LAB A-9 <LAB A-9@LABA-9>
2023-02-17 13:54:56 +0400

    Add project files.

---
+++ ConsoleApp2/EmptyArrayFiller.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class EmptyArrayFiller
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i<arr.Length; i++)
+            {
+                arr[i]+= i;
+            }
+            Console.WriteLine(arr[3]);
+        }
+    }
+}
